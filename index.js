ab533e14cbaed3e6a61d23bee731cd0d3490ce49
kevinmalats <ken_1721@hotmail.es>
2019-05-09 12:53:21 -0500

    h

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ switch (req.body.triggered_by) {
      pro= JSON.parse(req.body.resources);
   	console.log(pro.data);
       id= pro.data.id;
-    let data=pro.data;
+     const data=pro.data;
 		console.log("create");
 		try {
 		 body = await	Moltin.Products.Create(data).then(data => {
