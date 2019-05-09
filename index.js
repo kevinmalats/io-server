c1c043b8b524ac9a7c297dbe80bdfcc1c72f9913
kevinmalats <ken_1721@hotmail.es>
2019-05-09 10:30:04 -0500

    h

--- index.js
+++ index.js
@@ -24,8 +24,7 @@ app.get('/testNotification', (req, res) => {
 })
 
 app.post('/webhook', (req, res) => {
-  console.log("req.");
-	console.log(req);
+
 
 	console.log("req.triggered_by");
 	console.log(req.body.triggered_by);
